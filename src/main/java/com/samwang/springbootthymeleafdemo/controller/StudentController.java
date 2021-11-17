649a336c71d2ad12f26e6aec1af4dea6242d6dbd
王柏翔 <w32191@gmail.com>
2021-11-17 20:56:19 +0800

    student CRUD complete ID

--- src/main/java/com/samwang/springbootthymeleafdemo/controller/StudentController.java
+++ src/main/java/com/samwang/springbootthymeleafdemo/controller/StudentController.java
@@ -5,9 +5,12 @@ import com.samwang.springbootthymeleafdemo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class StudentController {
@@ -15,6 +18,7 @@ public class StudentController {
   @Autowired
   private StudentService studentService;
 
+  //取得全部學生資料
   @GetMapping("/students")
   public String getAllStudents(Model model) {
     Iterable<Student> studentsList = studentService.getAllStudents();
@@ -26,7 +30,7 @@ public class StudentController {
     return "studentlist";
   }
 
-
+  //新增一名學生資料
   @PostMapping("/students")
   public String createStudent(@ModelAttribute Student student, Model model) {
 
@@ -36,7 +40,35 @@ public class StudentController {
     model.addAttribute("studentList", allStudentList);
     model.addAttribute("studentObject", emptyStudent);
 
+    return "redirect:/students";
+  }
+
+  //刪除學生資料
+  @DeleteMapping("/student/{id}")
+  public String deleteStudent(@PathVariable Integer id, Model model) {
+    studentService.deleteById(id);
+    Student emptyStudent = new Student();
+    model.addAttribute("studentObject", emptyStudent);
+
     return "studentlist";
   }
+
+
+  //修改現有學生資料
+  @PostMapping("/editform")
+  public String editStudentForm(@ModelAttribute Student student, Model model) {
+    Student editedStudent = studentService.editStudent(student.getId(), student);
+    model.addAttribute("editedStudent", editedStudent);
+    return "editform";
+  }
+
+  //修改現有學生資料
+//  @PostMapping("/editform")
+//  public String editStudentForm(@RequestBody Student student, Model model) {
+//    Student editedStudent = studentService.editStudent(student.getId(), student);
+//    model.addAttribute("editedStudent", editedStudent);
+//    return "editform";
+//  }
+
 
 }
--- src/main/java/com/samwang/springbootthymeleafdemo/entity/Student.java
+++ src/main/java/com/samwang/springbootthymeleafdemo/entity/Student.java
@@ -7,7 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "student")
 public class Student {
